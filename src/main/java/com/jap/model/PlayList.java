865ad48048e03f05471152f8bfee02caa8ad3aca
aakash <kaushik.akash05@gmail.com>
2022-09-21 07:38:10 +0530

    created getters and setters in PlayList.java class

--- src/main/java/com/jap/model/PlayList.java
+++ src/main/java/com/jap/model/PlayList.java
@@ -11,4 +11,20 @@ public class PlayList {
         this.playlistId = playlistId;
         this.playlistName = playlistName;
     }
+
+    public int getPlaylistId() {
+        return playlistId;
+    }
+
+    public void setPlaylistId(int playlistId) {
+        this.playlistId = playlistId;
+    }
+
+    public String getPlaylistName() {
+        return playlistName;
+    }
+
+    public void setPlaylistName(String playlistName) {
+        this.playlistName = playlistName;
+    }
 }
