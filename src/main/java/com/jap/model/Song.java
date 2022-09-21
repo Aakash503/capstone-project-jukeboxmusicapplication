b85c661beb7e5577ef01ad53ea37f163a9a5917f
aakash <kaushik.akash05@gmail.com>
2022-09-21 07:22:17 +0530

    added fields in song class

--- src/main/java/com/jap/model/Song.java
+++ src/main/java/com/jap/model/Song.java
@@ -2,6 +2,7 @@ package com.jap.model;
 
 public class Song {
     private int songId;
+    private String songName;
     private String albumName;
     private String genre;
     private String songPath;
