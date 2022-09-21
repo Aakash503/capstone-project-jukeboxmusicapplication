c98a6fe0429d56514d00b77d651251ccc365bd7a
aakash <kaushik.akash05@gmail.com>
2022-09-21 10:11:43 +0530

    created dataservice object for Databaseservice class in main method and print connection status.

--- src/main/java/com/jap/Main.java
+++ src/main/java/com/jap/Main.java
@@ -1,7 +1,18 @@
 package com.jap;
 
+import com.jap.service.DatabaseService;
+
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
-
+        DatabaseService databaseService = new DatabaseService();
+        try {
+            databaseService.connect();
+        } catch (SQLException e) {
+            System.out.println("Error while connecting to the database");
+        } finally {
+            databaseService.printConnectionStatus();
+        }
     }
 }
