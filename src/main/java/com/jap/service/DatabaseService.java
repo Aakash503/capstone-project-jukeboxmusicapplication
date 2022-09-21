b764fb6839a9d81d3fbe1f8eea4db2c12fba5b6f
aakash <kaushik.akash05@gmail.com>
2022-09-21 08:19:34 +0530

    added fields url,username,passwords in DatabaseService.java class

--- src/main/java/com/jap/service/DatabaseService.java
+++ src/main/java/com/jap/service/DatabaseService.java
@@ -1,5 +1,10 @@
 package com.jap.service;
 
-public class DatabaseService {
+import java.sql.Connection;
 
+public class DatabaseService {
+    private final String Url = "jdbc:mysql://localhost:3306/jukebox";
+    private final String USERNAME = "root";
+    private final String PASSWORD = "Admin@123";
+    private Connection connection;
 }
