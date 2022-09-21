c4bdf987e071bd7157e1a9277ceb70b6605f3fdc
aakash <kaushik.akash05@gmail.com>
2022-09-21 07:42:24 +0530

    added equals,hashcode and tostring methods in PlayList.java class

--- src/main/java/com/jap/model/PlayList.java
+++ src/main/java/com/jap/model/PlayList.java
@@ -1,5 +1,7 @@
 package com.jap.model;
 
+import java.util.Objects;
+
 public class PlayList {
     private int playlistId;
     private String playlistName;
@@ -26,5 +28,23 @@ public class PlayList {
 
     public void setPlaylistName(String playlistName) {
         this.playlistName = playlistName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PlayList playList = (PlayList) o;
+        return playlistId == playList.playlistId && playlistName.equals(playList.playlistName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(playlistId, playlistName);
+    }
+
+    @Override
+    public String toString() {
+        return "PlayList{" + "playlistId=" + playlistId + ", playlistName='" + playlistName + '\'' + '}';
     }
 }
