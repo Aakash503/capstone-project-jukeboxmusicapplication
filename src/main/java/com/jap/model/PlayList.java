4b536db1c9507d21470acdae909dd654aff7f921
aakash <kaushik.akash05@gmail.com>
2022-09-21 07:36:05 +0530

    created constructors in PlayList.java class

--- src/main/java/com/jap/model/PlayList.java
+++ src/main/java/com/jap/model/PlayList.java
@@ -3,4 +3,12 @@ package com.jap.model;
 public class PlayList {
     private int playlistId;
     private String playlistName;
+
+    public PlayList() {
+    }
+
+    public PlayList(int playlistId, String playlistName) {
+        this.playlistId = playlistId;
+        this.playlistName = playlistName;
+    }
 }
