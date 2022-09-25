58f7dc70d1badc442856e3a5ebc0c87564ccdd93
aakash <kaushik.akash05@gmail.com>
2022-09-25 19:13:06 +0530

    added beforeEach setup and afterEach tearDown methods in PlaylistRepositoryTest.java class

--- src/test/java/PlaylistRepositoryTest.java
+++ src/test/java/PlaylistRepositoryTest.java
@@ -1,3 +1,24 @@
-public class PlaylistRepositoryTest {
+import com.jap.model.Song;
+import com.jap.repository.PlaylistRepository;
+import com.jap.service.DatabaseService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 
+public class PlaylistRepositoryTest {
+    PlaylistRepository playlistRepository = null;
+    Song song;
+    DatabaseService databaseService = new DatabaseService();
+
+    @BeforeEach
+    void setUp() {
+        playlistRepository = new PlaylistRepository();
+        song = new Song();
+        databaseService = new DatabaseService();
+    }
+
+    @AfterEach
+    void tearDown() {
+        playlistRepository = null;
+        song = null;
+    }
 }
